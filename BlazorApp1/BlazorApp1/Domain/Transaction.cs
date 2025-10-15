124a6825337dc1f588adf0ca657a4929bcb0ea15
Mcfluffelbl <linnea.jmb@gmail.com>
2025-10-15 10:27:51 +0200

    Lagt till delete button och påbörjade Transaction

--- BlazorApp1/BlazorApp1/Domain/Transaction.cs
+++ BlazorApp1/BlazorApp1/Domain/Transaction.cs
@@ -2,5 +2,21 @@
 {
     public class Transaction
     {
+        public int Id { get; set; }
+        public Guid AccountId { get; set; }
+        public TransactionType TransactionType { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public Transaction(Guid accountId, TransactionType transactionType, decimal amount, DateTime date, string description, string status)
+        {
+            AccountId = accountId;
+            TransactionType = transactionType;
+            Amount = amount;
+            Date = date;
+            Description = description;
+            Status = status;
+        }
     }
 }
---
+++ BlazorApp1/BlazorApp1/Domain/TransactionType.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorApp1.Domain
+{
+    public enum TransactionType
+    {
+        Deposit,
+        Withdrawal,
+        Transfer
+    }
+}
