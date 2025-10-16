4d3773e2aacf9d8577d7b528968aaaeef5222573
Mcfluffelbl <linnea.jmb@gmail.com>
2025-10-17 00:08:34 +0200

    börjat med transaktion

--- BlazorApp1/BlazorApp1/Domain/Transaction.cs
+++ BlazorApp1/BlazorApp1/Domain/Transaction.cs
@@ -2,5 +2,21 @@
 {
     public class Transaction
     {
+<<<<<<< Updated upstream
+=======
+        public Guid AccountId { get; set; }
+        public TransactionType TransactionType { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public Transaction(Guid accountId, TransactionType transactionType, decimal amount, DateTime date, string description)
+        {
+            AccountId = accountId;
+            TransactionType = transactionType;
+            Amount = amount;
+            Date = date;
+            Description = description;
+        }
+>>>>>>> Stashed changes
     }
 }
